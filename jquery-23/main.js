0db282209921af43032a183f7173f1a54dae7cb2
Miguel Blanco <lmiguelblancoc@gmail.com>
2020-11-23 20:37:59 -0500

    console

---
+++ jquery-23/main.js
@@ -0,0 +1,21 @@
+let nombre = "Luis Miguel Blanco";
+let edad = "25";
+let correo = "lmiguelblancoc@gmail.com";
+let trabajo = "Comfenalco Santander";
+let genero = "Masculino";
+let altura = 1.67;
+let peso = 50;
+let color_pelo = "marron";
+let piel = "blanca";
+
+
+
+console.log("Mi nombre es: " +nombre);
+console.log("Mi edad es: " +edad);
+console.log("Mi correo electronico : " +correo);
+console.log("Trabajo en : " +trabajo);
+console.log("Genero : " +genero);
+console.log("Mi altura es: " +altura);
+console.log("Mi peso es: " +peso+ " Kg");
+console.log("Mi color de pelo es: " +color_pelo);
+console.log("Mi color de piel es: " +piel);
