9f0ff6f2e1f384be9acd6289c6c7d9c9beac4060
Miguel Blanco <lmiguelblancoc@gmail.com>
2020-11-25 18:56:44 -0500

    nuevo 25

---
+++ Js_array_listas_25_11_2020/main.js
@@ -0,0 +1,48 @@
+
+// Ingresar datos por medio de la ventana de javascript
+// Listas
+// let lista_informacion = [];
+
+// let nombre = prompt("Ingrese su nombre");
+// lista_informacion.push(nombre);
+
+// let apellido = prompt("Ingrese su apellido");
+// lista_informacion.push(apellido);
+
+// console.log(lista_informacion);
+
+
+// **************************************
+// let lista_informacion = [];
+
+// let nombre = prompt("Ingrese su nombre");
+// lista_informacion["nombre"] = nombre;
+// let apellido = prompt("Ingrese su apellido");
+// lista_informacion["apellido"] = apellido;
+
+// console.log(lista_informacion);
+
+// **************************************
+// let lista_informacion = {};
+
+// let nombre = prompt("Ingrese su nombre");
+// lista_informacion.NOMBRE = nombre;
+// let apellido = prompt("Ingrese su apellido");
+// lista_informacion.APELLIDO = apellido;
+
+// console.log(lista_informacion);
+
+
+// **************************************
+let lista_informacion = {};
+let lista_informacion2 = [];
+
+let nombre = prompt("Ingrese su nombre");
+lista_informacion.NOMBRE = nombre;
+lista_informacion2.NOMBRE = nombre;
+let apellido = prompt("Ingrese su apellido");
+lista_informacion.APELLIDO = apellido;
+lista_informacion2.APELLIDO = apellido;
+
+console.log(lista_informacion);
+console.log(lista_informacion2);
