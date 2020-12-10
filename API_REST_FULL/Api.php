0359f7873e84755290b561921bf03fa77987c66c
lmblanco <lmiguelblancoc@gmail.com>
2020-12-10 11:15:26 -0500

    10

--- API_REST_FULL/Api.php
+++ API_REST_FULL/Api.php
@@ -1,18 +1,39 @@
 <?php
 
-$prueba ='';
-class Crud{
-    public function __construct($arg){
+// class Seguridad(){
+//     protected function Token($token){
+//         if ($token == "123") {
+//             return true;
+//         }else{
+//             return false;
+//         }
+
+//     }
+
+// }
+
+
+class Crud extends Seguridad {
+    private $datos;
+    public function __construct($arg,$token){
         $this->datos = $arg;
+        if ($this->Token($token)) {
+            $this->Guardar();
+        } 
     }
 
+    protected function Guardar(){
+        $f = fopen("datos.json", "w+");
+        fwrite($f, json_decode($this->datos));
+        fclose($f);
+    }
     
 }
 
 if($_SERVER['REQUEST_METHOD'] == "POST"){
-    $obj = new informacion($_SERVER['HTTP_CLASE']($_POST['datos'],$_SERVER['HTTP_GUARDAR_PHP']));
+    $obj = new $_SERVER["HTTP_CLASE"]($_POST["datos"],$_SERVER["HTTP_GUARDAR_PHP"]);
 }else{
-    echo "No se ha creado el Epoint";
+    echo "No se ha creado el Epoint por el metodo".$_SERVER["REQUEST_METHOD"];
 
 
 }
--- API_REST_FULL/main.js
+++ API_REST_FULL/main.js
@@ -1,16 +1,18 @@
+
 $(document).ready(function () {
     $("#enviar").click(function (e) {
         e.preventDefault();
         let datos = [];
         datos.push($("#input_nombre").val());        
+        datos.push($("#input_edad").val());
         datos.push($("#input_direccion").val());
-        datos.push($("#input_genero").val());
         
         let obj = new informacion();
         obj.Guardar(datos);
 
       });
 });
+
 
 
 class informacion{
@@ -21,7 +23,8 @@ class informacion{
             url: "Api.php",
             data: {datos},
             header: {
-                "guardar-php" : "Crud"
+                "clase" : "Crud",
+                "guardar-php" : "123"
             },
 
             success: function (response) {
